5b720d3f113211eb3937ad951e50ab60f375a227
joao <druvao136@gmail.com>
2023-05-22 16:55:37 -0300

    req1 completo

---
+++ src/redux/reducer/player.js
@@ -0,0 +1,20 @@
+import { SAVE_NAME } from '../actions';
+
+const INITIAL_STATE = {
+  player: {
+    name: '',
+    assertions: '',
+    score: '',
+    gravatarEmail: '',
+  },
+};
+
+const playerReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case (SAVE_NAME):
+    return ({ ...state, name: payload });
+  default: return state;
+  }
+};
+
+export default playerReducer;
