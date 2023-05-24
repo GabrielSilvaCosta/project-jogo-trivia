dbf8891ea36522d5698d674994fd8c5f8c2710ba
GabrielSilvaCosta <bob@>
2023-05-24 19:06:20 -0300

    requisito 6 e 7

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,73 +1,140 @@
 import React, { Component } from 'react';
-import { getQuest } from '../services/getApi';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default class Game extends Component {
+class Game extends Component {
   state = {
     questions: [],
-    currentQuestionIndex: 0,
+    clicked: false,
+    questionIndex: 0,
   };
 
-  async componentDidMount() {
-    const response = await getQuest();
-    if (response && response.length > 0) {
-      const questions = response.map((question) => ({
-        ...question,
-        options: this.shuffleOptions(this.getOptions(question)),
-      }));
-      this.setState({ questions });
-    }
+  componentDidMount() {
+    this.requestQuestions();
   }
 
-  getOptions = (question) => {
-    if (question.type === 'boolean') {
-      return [question.correct_answer, 'True'];
+  requestQuestions = async () => {
+    const { history } = this.props;
+    const token = localStorage.getItem('token');
+    try {
+      const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+      const data = await response.json();
+
+      if (data.results.length === 0) {
+        localStorage.clear();
+        return history.push('/');
+      }
+
+      const questions = data.results.map((question) => {
+        const options = [question.correct_answer, ...question.incorrect_answers];
+        const shuffledOptions = this.shuffleArray(options);
+        question.options = shuffledOptions;
+        return question;
+      });
+
+      return this.setState({ questions, clicked: false });
+    } catch (error) {
+      console.error('Faça login novamente!', error);
+      localStorage.clear();
+      return history.push('/');
     }
-    return [question.correct_answer, ...question.incorrect_answers];
   };
 
-  shuffleOptions = (options) => {
-    const shuffledOptions = [...options];
-    for (let i = shuffledOptions.length - 1; i > 0; i -= 1) {
+  // botao para a proxima pergunta
+
+  renderButtonClick = () => {
+    const { clicked } = this.state;
+    if (clicked) {
+      return (
+        <button data-testid="btn-next" onClick={ this.nextButtonClick }>Next</button>
+      );
+    }
+  };
+
+  nextButtonClick = () => {
+    const { questionIndex, questions } = this.state;
+    if (questionIndex + 1 < questions.length) {
+      this.setState((prevState) => ({
+        questionIndex: prevState.questionIndex + 1,
+        clicked: false,
+      }));
+    }
+  };
+
+  shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledOptions[i], shuffledOptions[j]] = [shuffledOptions[j], shuffledOptions[i]];
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
     }
-    return shuffledOptions;
-  };
-
-  handleNextQuestion = () => {
-    this.setState((prevState) => ({
-      currentQuestionIndex: prevState.currentQuestionIndex + 1,
-    }));
-  };
+    return shuffledArray;
+  }
 
   render() {
-    const { questions, currentQuestionIndex } = this.state;
+    const { questions, clicked, questionIndex } = this.state;
 
     if (questions.length === 0) {
-      return <div>Loading...</div>;
+      return <div data-testid="loading">Loading...</div>;
     }
 
-    const currentQuestion = questions[currentQuestionIndex];
+    const {
+      category, question, options, correct_answer: correct,
+    } = questions[questionIndex];
 
     return (
-      <>
+      <div>
         <Header />
-        <div>Game</div>
-        <div data-testid="question-category">{currentQuestion.category}</div>
-        <div data-testid="question-text">{currentQuestion.question}</div>
-        <div data-testid="answer-options">
-          {currentQuestion.options.map((option, index) => (
-            <button
-              key={ index }
-              data-testid={ index === 0 ? 'correct-answer' : `wrong-answer-${index - 1}` }
-            >
-              {option}
-            </button>
-          ))}
+        <div>
+          <div>
+            <p data-testid="question-category">{category}</p>
+            <p data-testid="question-text">{question}</p>
+            <div data-testid="answer-options">
+              {options.map((option, optionIndex) => {
+                if (option === correct) {
+                  return (
+                    <button
+                      key={ optionIndex }
+                      data-testid="correct-answer"
+                      onClick={ () => this.setState({ clicked: true }) }
+                      style={ clicked ? { border: '3px solid rgb(6, 240, 15)' } : null }
+                    >
+                      {option}
+                    </button>
+                  );
+                }
+                return (
+                  <button
+                    key={ optionIndex }
+                    data-testid={ `wrong-answer-${optionIndex}` }
+                    onClick={ () => this.setState({ clicked: true }) }
+                    style={ clicked ? { border: '3px solid red' } : null }
+                  >
+                    {option}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+
+          {this.renderButtonClick()}
+
         </div>
-        <button onClick={ this.handleNextQuestion }>Next Question</button>
-      </>
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  state: state.player,
+});
+
+Game.propTypes = {
+  state: PropTypes.shape({
+    name: PropTypes.string,
+    email: PropTypes.string,
+    score: PropTypes.number,
+  }),
+}.isRequired;
+
+export default connect(mapStateToProps)(Game);
