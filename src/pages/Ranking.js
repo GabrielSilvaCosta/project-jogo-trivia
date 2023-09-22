649a09557f6fe7808d2f2ed53d0f450a9f5fe5d5
GabrielSilvaCosta <bob@>
2023-09-22 09:15:34 -0300

    atualizandooo

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,31 +1,51 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
+import Header from '../components/Header';
+import { saveName } from '../redux/actions/index';
 
 class Ranking extends React.Component {
+  componentDidMount() {
+    const { player, score, gravatar } = this.props;
+    const profile = {
+      urlImage: `https://www.gravatar.com/avatar/${gravatar}`,
+      player,
+      score,
+    };
+    const localStorageData = localStorage.getItem('Ranking');
+    const profileList = localStorageData ? JSON.parse([localStorageData]) : [];
+    profileList.push(profile);
+    saveName(profileList);
+  }
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/');
   };
 
   render() {
-    const userData = JSON.parse(localStorage.getItem('ranking'));
+    const userData = JSON.parse(localStorage.getItem('Ranking'));
     const dataSorted = userData ? [...userData]
       .sort((first, second) => second.score - first.score) : [];
 
     return (
       <div>
+        <Header />
         <div>
           <h1 data-testid="ranking-title">Ranking</h1>
           {
             dataSorted.map((user, index) => (
               <div key={ user.name }>
                 <img src={ user.picture } alt={ user.name } />
-                <span data-testid={ `player-name-${index}` }>{user.name}</span>
-                <span data-testid={ `player-score-${index}` }>
+                <p data-testid={ `player-name-${index}` }>{user.name}</p>
+                <p>
+                  {' '}
+                  data-testid=
+                  { `player-score-${index}` }
                   {user.score}
                   {' '}
                   pontos
-                </span>
+                </p>
               </div>
             ))
           }
